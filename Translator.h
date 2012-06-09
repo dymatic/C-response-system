ba35fabd6d3df6b71fbc5ab182a62e1e06f861de
Norton Jenkins <nate@Phoenix.(none)>
2012-06-09 15:11:07 -0400

    Updated translator to 2.4

--- Translator.h
+++ Translator.h
@@ -1,29 +1,30 @@
-
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include <sstream>
-//Whole file copied directly from Langdiff. Major versions are reflected
-//Here when released.
+
 using namespace std;
 
 string *fromStrings=new string[10000];//C++ members
 string *toStrings=new string[10000]; // Custom language members
 void splitstr(string &c,string s);
+void replaceAll(std::string& str,  std::string& from,  std::string& to);
 int linec(string);
 int members=0;                     //  The number of members
 
 /**
 *Orders the langdefines by line length.
 *This prevents the replacing of smaller words in
-*larger ones.
+*larger ones. This is an exact copy of the function in NSL,
+*with the addition of title checking.
 *@param filepath - The path to the file
+*@param ltor - True (1) if the largest goes first, false (0) otherwise.
 */
-void orderFile(string filepath)
+void orderFile(string filepath, bool ltor)
 {
     ifstream file(filepath.c_str());
     int nlines=linec(filepath);
-
+    ofstream writer(filepath.c_str());
     string lines[nlines];//Contains all of the lines
 
     for(int index=0; index<nlines; index++) //Make container
@@ -31,21 +32,50 @@ void orderFile(string filepath)
 
     for(int index=0; index<nlines-1; index++) //Iterate through positions
     {
-        while(lines[index].length()<lines[index+1].length())
+        if(ltor)
         {
-            string buffer;//Loop executes while a string below it needs replacing.
-            buffer=lines[index];
+            if(lines[index].find("$")!=-1&&lines[index].find("@")==-1)
+                    splitstr(lines[index],"$");//Cuts the comment if the allow character is not present.
 
-            lines[index]=lines[index+1];
-            lines[index+1]=buffer;
-            cout << "Let's see something."<<endl;
-            index--;
+            while(lines[index].length()<lines[index+1].length())
+            {//Checks the line before it, changes them, and subtracts one from index.
+                bool title=false;//Whether or not it is a title line (===)
+                string buffer=lines[index];
+
+                if(lines[index].find("===")!=-1)
+                    title=true;
+
+                lines[index]=lines[index+1];
+                lines[index+1]=buffer;
+                index--;
+
+                if(title&&index==0)
+                    break;//Will not enroach on the title, despite size.
+            }
         }
-    }
+        else
+        {
+            while(lines[index].length()>lines[index+1].length())
+            {
+               //Checks the line before it, changes them, and subtracts one from index.
+                bool title=false;//Whether or not it is a title line (===)
+                string buffer=lines[index];
+
+                if(lines[index].find("===")!=-1)
+                    title=true;
+
+                lines[index]=lines[index+1];
+                lines[index+1]=buffer;
+                index--;
+
+                if(title&&index==0)
+                    break;//Will not enroach on the title, despite size.
+            }//End while for backwards
+        }//End else that determines whether or not to do it forwards or backwards.
+        writer<<lines[index]<<endl;//Write the ordered array
+    }//End main for
+
     file.close();
-    ofstream writer(filepath.c_str());
-    for(int index=0; index<nlines; index++)
-        writer<<lines[index]<<endl;//Write the ordered awway
     writer.close();
 }
 /**
@@ -59,6 +89,8 @@ void loadLanguage(string filename,int lines)
 {
     string buffer; //Used for parsing individual lines
     members=lines;// How many entries the langdefines has
+     string title="===";//Used for language titles on the first line of a langdefines
+    string replaceWith="Language Title: ";
     int until=0; /*Until is used to determine how many NO_USE characters to use.
                     NO_USE characters show an unwritten line.*/
 
@@ -66,27 +98,40 @@ void loadLanguage(string filename,int lines)
     for(int index=0; index<10000; index++)
     {
         getline(file,buffer);
-        if(buffer.find("$")!=-1)
-            splitstr(buffer,"$");
-
+        if(buffer.find("$")!=-1&&buffer.find("@")==-1) //String is split if let_symbol is here
+        {
+            splitstr(buffer,"$");//Replace in between the comments
+        }
+        else if(buffer.find("@")!=-1) //Let symbol is deleted
+        {
+            buffer.replace(buffer.find("@"),buffer.find("@"),"");
+        }
+        if(buffer.find("===")!=-1&&index==0)
+        {
+            replaceAll(buffer,title,replaceWith);
+            cout << buffer<<endl;
+            index--;
+            continue;
+        }
+        //This marks the end of all of the file syntax and the beginning of the real language scraping.
         if(buffer.find("_")!=-1)
         {
-            toStrings[index]=buffer.substr(0,buffer.find("_"));
+            toStrings[index]=buffer.substr(0,buffer.find("_"));//Tostrings is first half of underscore
 
-            fromStrings[index]=buffer.substr(buffer.find("_")+1,buffer.length());
+            fromStrings[index]=buffer.substr(buffer.find("_")+1,buffer.length());//From is last half of underscore
+
         }
         else
         {
-            until=10000-index;//Second bug
+            until=10000-index;//How many null characters to write
             break;
         }
-
-        for(int index=10000-until; index<10000; index++)
-        {
-            //\++10x is used to indicate a line not being used.
-            toStrings[index]="/++10x";
-            fromStrings[index]="/++10x";
-        }
+    }
+    for(int index=10000-until; index<10000; index++)
+    {
+        //\++10x is used to indicate a line not being used.
+        toStrings[index]="/++10x";
+        fromStrings[index]="/++10x";
     }
 }
 /**
@@ -98,12 +143,12 @@ int linec(string filename)
 {
     ifstream file(filename.c_str());
     int lines=0;
-    string buffer;
+    string *buffer=new string;
 
-    for(; getline(file,buffer);)
-    {
+    for(; getline(file,*buffer);)
         lines++;
-    }
+
+    file.close();
     return lines;
 }
 /**
@@ -119,6 +164,7 @@ void replaceAll(std::string& str,  std::string& from,  std::string& to)
     {
         str.replace(start_pos, from.length(), to);
         start_pos += to.length();
+
     }
 }
 /**
@@ -136,27 +182,22 @@ void splitstr(string &toSplit, string magichar)
 {
     if(toSplit.find(magichar)!=-1)
     {
-
         stringstream buffer;
 
         string copy=toSplit;
-        string cnholder="";
-        string nope="";
+        string nope="";//replaceall needs a reference.
 
+        if(toSplit.find(magichar)!=-1)//Put the text before first magichar in buffer
+            buffer<<copy.substr(0,toSplit.find(magichar));
 
-        if(toSplit.find(magichar)!=-1)
-            buffer<<copy.substr(0,toSplit.find(magichar));//Before first magichar
-
-        cnholder=buffer.str();
-
-        if(copy.find(magichar)!=-1)
+        if(copy.find(magichar)!=-1)//Make copy equal to the whole string minues the part up to the first magichar
             copy=copy.substr(toSplit.find(magichar)+1,toSplit.length());
 
-        if(copy.find(magichar)!=-1)
-            buffer<<copy.substr(copy.find(magichar)+1,copy.length());//Substring of one more than the next magichar and the length.
+        if(copy.find(magichar)!=-1)//Put the position of the magichar+1 in the buffer.
+            buffer<<copy.substr(copy.find(magichar)+1,copy.length());
 
         toSplit=buffer.str();
-    }
+    }//End if
 }
 /**
  * Creates a file containing the old text as defined by
@@ -171,47 +212,90 @@ void splitstr(string &toSplit, string magichar)
  */
 void compile(string fileName,int lines,string toWriteTo,string magichar)
 {
-
     string line[lines];
     ifstream reader(fileName.c_str());
+    ofstream writer(toWriteTo.c_str());
+    cout << lines<<endl;
 
-    for(int index=0; index<lines; index++)
-    {
-        getline(reader, line[index]);
-    }
 
     for(int index=0; index<lines; index++) // Loops through each line
     {
+        getline(reader, line[index]);
         for(int secInd=0; secInd<members; secInd++)//Performs replacement operations
         {
             splitstr(line[index],magichar); // splitstr removes protected strings from lines[index] IF they exist.
 
-            if(fromStrings[secInd]!="/++10x"&&toStrings[secInd]!="/++10x")
+            if(fromStrings[secInd]!="/++10x"&&toStrings[secInd]!="/++10x")//NULL string to ensure quality.
             {
                 replaceAll(line[index],fromStrings[secInd],toStrings[secInd]);
             }
 
-        }
-        cout << line[index]<<endl;
-        reader.close();
-    }
-    ofstream writer(toWriteTo.c_str());
+        }//END for replacements
 
-    for(int index=0; index<lines; index++)//Iterate through the changed lines and write them to a file.
-    {
         writer << line[index]<<endl;
+
     }
     writer.close();
+    reader.close();
+}
+/**
+*Compiles a set of files in a Noran Make File.
+*iCompile stands for Iterative Compilation
+*The syntax for a Noran Make File is infile_outfile_magichar
+*@param nmfPath - The path to the Noran Make File
+*/
+void iCompile(string nmfPath)
+{
+    ifstream nmf(nmfPath.c_str());
+    int nmflength = linec(nmfPath);
+
+    for(int index=0; index<nmflength; index++)
+    {
+        string buffer;
+        string inFile, outFile, magichar;
+
+        getline(nmf,buffer);
+        //in_out_mag
+        inFile=buffer.substr(0,buffer.find("_"));
+        buffer=buffer.substr(buffer.find("_")+1,buffer.length());
+        //out_mag
+        outFile=buffer.substr(0,buffer.find("_"));
+        buffer=buffer.substr(buffer.find("_")+1,buffer.length());
+        //mag
+        magichar=buffer;
+        compile(inFile,linec(inFile.c_str()),outFile,magichar);
+    }
 }
 /**
 *Runs the program
 */
 void transRun(){
-    string argv2[6];
-    cout << "What is the total file path to the language definition file?"<<endl<<"$ ";
+ string argv2[6];
+        char choice='a';
+        cout << "What is the total file path to the language definition file?"<<endl<<"$ ";
         cin  >> argv2[0];
         cout << endl;
 
+        cout << "Would you like the langdefines automatically ordered? y or n "<<endl<<"$ ";
+        cin  >> argv2[4];
+        cout << endl;
+
+        cout << "Are you using a Noran Make File? y/n"<<endl<<"$ ";
+        cin  >> choice;
+        cout << endl;
+
+        if(choice=='y')
+            {
+                if(argv2[4].find("Y")!=-1)
+                    orderFile(argv2[0],true);
+
+                string filename;
+                cout << "What is the path to the Noran Make File?"<<endl<<"$ ";
+                cin  >> filename;
+                cout << endl;
+
+                iCompile(filename);
+            }else{
         cout << "What is the total file path to the code?"<<endl<<"$ ";
         cin  >> argv2[1];
         cout << endl;
@@ -224,15 +308,11 @@ void transRun(){
         cin  >> argv2[3];
         cout << endl;
 
-        cout << "Would you like the langdefines automatically ordered? y/m "<<endl<<"$ ";
-        cin  >> argv2[4];
-        cout << endl;
-
+    }
     if(argv2[4].find("y")!=-1)
-        orderFile(argv2[0]);
+        orderFile(argv2[0],true);
 
     loadLanguage(argv2[0],linec(argv2[0]));
     compile(argv2[1],linec(argv2[1]),argv2[2],argv2[3]);
-
     cout << endl;
 }
